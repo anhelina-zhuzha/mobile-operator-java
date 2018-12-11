489988d312fc5e73a9b41e2380e678f00cc0c22f
Anhelina Zhuzha <ang@mail>
2018-12-11 22:53:34 +0200

    use Spring framework

--- src/main/java/com/anhelinaZhuzha/mobileOperator/MobileOperatorApplication.java
+++ src/main/java/com/anhelinaZhuzha/mobileOperator/MobileOperatorApplication.java
@@ -1,35 +1,12 @@
 package com.anhelinaZhuzha.mobileOperator;
 
-import com.anhelinaZhuzha.mobileOperator.model.Tariff;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.anhelinaZhuzha.mobileOperator.model.storage.FileStorage;
-import com.anhelinaZhuzha.mobileOperator.model.storage.StorageInterface;
-
-import java.util.Iterator;
-import java.util.Map;
-
+@SpringBootApplication
 public class MobileOperatorApplication {
 
     public static void main(String[] args) {
-        StorageInterface storage = new FileStorage();
-
-        Tariff student = storage.getByName("student");
-
-        System.out.println("Tariff: " + student.name());
-        System.out.println("Price: " + student.finalPrice());
-        System.out.println("Services: \n" + student.servicesInfo());
-
-
-        Map<String, Tariff> tariffs = storage.getAll();
-        Iterator it = tariffs.entrySet().iterator();
-        while (it.hasNext()) {
-
-            Map.Entry pair = (Map.Entry)it.next();
-            Tariff tariff = (Tariff) pair.getValue();
-            System.out.println("Tariff: " + tariff.name());
-            System.out.println("Price: " + tariff.finalPrice());
-            System.out.println("Services: \n" + tariff.servicesInfo());
-            System.out.println("\n");
-        }
+        SpringApplication.run(MobileOperatorApplication.class, args);
     }
 }
